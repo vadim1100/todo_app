b71a03b28bd78c9f4b0a0350d27c5d4ba387e7e0
Vadim <vadimnoviyakk@mail.ru>
2026-09-12 13:53:32 +0300

    изменил миграции для будущей работоспособности приложения

--- migrations/000001_init.down.sql
+++ migrations/000001_init.down.sql
@@ -1,2 +1,3 @@
 DROP TABLE IF EXISTS todoapp.tasks;
+DROP TABLE IF EXISTS todoapp.users;
 DROP SCHEMA IF EXISTS todoapp;
--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -1,11 +1,20 @@
 CREATE SCHEMA IF NOT EXISTS todoapp;
 
+CREATE TABLE IF NOT EXISTS todoapp.users (
+    id              SERIAL              PRIMARY KEY,
+    version         BIGINT          NOT NULL DEFAULT 1,
+    username        VARCHAR(200)    UNIQUE NOT NULL,
+    password_hash   VARCHAR(300)    NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS todoapp.tasks (
-    id SERIAL       PRIMARY KEY,
-    title           VARCHAR(100) NOT NULL CHECK(char_length(title) BETWEEN 1 AND 100),
-    description     VARCHAR(500) CHECK(char_length(description) BETWEEN 1 AND 500),
-    completed       BOOLEAN NOT NULL,
-    created_at      TIMESTAMPTZ NOT NULL,
+    id              SERIAL              PRIMARY KEY,
+    version         BIGINT          NOT NULL DEFAULT 1,
+    user_id         INTEGER         NOT NULL REFERENCES todoapp.users(id),
+    title           VARCHAR(100)    NOT NULL CHECK(char_length(title) BETWEEN 1 AND 100),
+    description     VARCHAR(500)    CHECK(char_length(description) BETWEEN 1 AND 500),
+    completed       BOOLEAN         NOT NULL,
+    created_at      TIMESTAMPTZ     NOT NULL,
     completed_at    TIMESTAMPTZ,
 
     CHECK (
@@ -14,3 +23,5 @@ CREATE TABLE IF NOT EXISTS todoapp.tasks (
         (completed=TRUE AND completed_at IS NOT NULL AND completed_at >= created_at)
     )
 );
+
+CREATE INDEX idx_tasks_user_id ON todoapp.tasks(user_id);
